5c29820c1464e63f8cfc113e1650140f4b7312fe
Vinay React <vinaysahuwe2020@gmail.com>
2022-05-01 17:16:04 +0530

    added navbar and created page commponent

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navs from "./Commponents/Navs";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
+function App() {
+  return (
+    <div>
+      <Navs />
+      <switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+      </switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Starred = () => {
+  return <div>This is STARRED PAGE</div>;
+};
+
+export default Starred;
