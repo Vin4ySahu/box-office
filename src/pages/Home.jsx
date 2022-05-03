530a78a4aa0dbad85e2566ea832dac68d26f7054
Vinay React <vinaysahuwe2020@gmail.com>
2022-05-03 15:27:15 +0530

    created input and feetch result form TV mazw API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import MainPagelayout from "../Commponents/MainPagelayout";
 
 const Home = () => {
-  return <MainPagelayout>This is HOME PAGE</MainPagelayout>;
+  const [input, setInput] = useState("");
+  const onInputChange = (ev) => {
+    setInput(ev.target.value);
+  };
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then((r) => r.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = (ev) => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+  return (
+    <MainPagelayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        value={input}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPagelayout>
+  );
 };
 
 export default Home;
