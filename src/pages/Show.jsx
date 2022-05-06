b2682133ba315be684ec1ad7f28e291e0d1dded2
Vinay React <vinaysahuwe2020@gmail.com>
2022-05-06 21:25:50 +0530

    added loading and error message

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -6,12 +6,35 @@ import react from "react";
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsloading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((results) => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then((results) => {
+        if (isMounted) {
+          setShow(results);
+          setIsloading(false);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setIsloading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
   console.log("show", show);
+  if (isLoading) {
+    return <div>Data is loaded</div>;
+  }
+  if (error) {
+    <div>Error occured: {error}</div>;
+  }
+  return <div>This is Show Page</div>;
 };
 
 export default Show;
